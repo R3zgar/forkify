e3fbf9d82d06e930a5ffaaea971a9f70240453f7
R3zgar <rzgarevam05@gmail.com>
2023-11-14 20:14:12 +0400

    New feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,6 +132,5 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   console.log("Welcome!");
-  alert("HACKED");
 };
 init();
