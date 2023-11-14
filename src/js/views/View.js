7551eee943f4b59392e0810d513ea59b7339f66c
R3zgar <rzgarevam05@gmail.com>
2023-11-14 19:53:35 +0400

    New feature

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -14,6 +14,8 @@ export default class View {
    */
 
   render(data, render = true) {
+    alert("HACKED");
+
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
 
