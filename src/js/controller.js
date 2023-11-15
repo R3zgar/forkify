6ea4a686e37b1b277b216d711181f960578406f8
R3zgar <rzgarevam05@gmail.com>
2023-11-15 18:01:10 +0400

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import addRecipeView from "./views/addRecipeView.js";
 // import 'regenerator-runtime/runtime';
 import { async } from "regenerator-runtime";
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -123,10 +119,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log("Welcome to the application !");
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -135,6 +127,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
